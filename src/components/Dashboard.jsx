86c800c487ae45931a797623d4851493aa4e67dd
Olamide <olamide.thinker@gmail.com>
2023-07-27 01:37:24 +0100

    updated

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,7 +1,7 @@
 
 import styled from "@emotion/styled";
 import { TimeCard } from "./deshboardComponents/TimeCard";
-import axios from 'axios';
+// import axios from 'axios';
 import profileImage from "./../assets/images/image-jeremy.png";
 import { ProfileCard } from "./deshboardComponents/ProfileCard";
 
@@ -15,6 +15,127 @@ import iconWork from "./../assets/images/icon-work.svg";
 import { useEffect, useState } from "react";
 
 
+const DashBoardData = () => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve({
+        data: {
+          dashboard: [
+            {
+              "id": 1,
+              "title": "Exercise",
+              "topColor": "var(--Limegreen)",
+                  "daily": {
+                "hoursSpent": "7hrs",
+                "timeframe": "Yesterday - 2hrs"
+              },
+              "weekly": {
+                "hoursSpent": "8hrs",
+                "timeframe": "Last Week - 122hrs"
+              },
+              "monthly": {
+                "hoursSpent": "12hrs",
+                "timeframe": "Last Month - 190hrs"
+              }
+            },
+            {
+              "id": 2,
+              "title": "Play",
+              "topColor": "var( --Softblue)",
+                   "daily": {
+                "hoursSpent": "10hrs",
+                "timeframe": "Yesterday - 5hrs"
+              },
+              "weekly": {
+                "hoursSpent": "2hrs",
+                "timeframe": "Last Week - 132hrs"
+              },
+              "monthly": {
+                "hoursSpent": "8hrs",
+                "timeframe": "Last Month - 209hrs"
+              }
+            },
+            {
+              "id": 3,
+              "title": "Self Care",
+              "topColor": "var( --Softorange)",
+                "daily": {
+                "hoursSpent": "156hrs",
+                "timeframe": "Yesterday - 54hrs"
+              },
+              "weekly": {
+                "hoursSpent": "24hrs",
+                "timeframe": "Last Week - 32hrs"
+              },
+              "monthly": {
+                "hoursSpent": "85hr",
+                "timeframe": "Last Month - 2109hrs"
+              }
+            },
+            {
+              "id": 4,
+              "title": "Social",
+              "topColor": "var( --Violet)",
+                    "daily": {
+                "hoursSpent": "123hrs",
+                "timeframe": "Yesterday - 57hrs"
+              },
+              "weekly": {
+                "hoursSpent": "1hr",
+                "timeframe": "Last Week - 12hrs"
+              },
+              "monthly": {
+                "hoursSpent": "80hrs",
+                "timeframe": "Last Month - 200hrs"
+              }
+            },
+            {
+              "id": 5,
+              "title": "Work",
+              "topColor": "var( --Lightorange)",
+                   "daily": {
+                "hoursSpent": "180hrs",
+                "timeframe": "Yesterday - 15hrs"
+              },
+              "weekly": {
+                "hoursSpent": "29hrs",
+                "timeframe": "Last Week - 192hrs"
+              },
+              "monthly": {
+                "hoursSpent": "18hrs",
+                "timeframe": "Last Month - 229hrs"
+              }
+            },
+            {
+              "id": 6,
+              "title": "Study",
+              "topColor": "var( --Lightred)",
+              
+              "daily": {
+                "hoursSpent": "90hrs",
+                "timeframe": "Yesterday - 5hrs"
+              },
+              "weekly": {
+                "hoursSpent": "12hrs",
+                "timeframe": "Last Week - 2hrs"
+              },
+              "monthly": {
+                "hoursSpent": "8hrs",
+                "timeframe": "Last Month - 9hrs"
+              }
+            }
+          ],
+        },
+      });
+    }, 1000); // Simulate 1 second delay to mimic server response time
+  });
+};
+
+
+
+
+
+
 const Dashboard = () => {
   const userfirstname = 'Jeremy';
   const userlastname = 'Robson';
@@ -22,11 +143,13 @@ const Dashboard = () => {
   
   const [data, setData] = useState();
 
-  // Fetching the data from the DB
+  // Fetching the data from the test API
   const fetchData = async () => {
     try {
-      const res = await axios.get('http://localhost:3000/dashboard');
-      setData(res.data);
+      const res = await DashBoardData();
+      console.log(res.data.dashboard)
+      setData(res.data.dashboard);
+
     } catch (err) {
       console.log('Failed:', err);
     }
@@ -41,9 +164,6 @@ const Dashboard = () => {
   const getData = (value) => {
     setPeriod(value);
   }
-
-  
-
 
 
   return (
